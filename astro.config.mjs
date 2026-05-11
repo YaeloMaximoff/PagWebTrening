4e2040f8147fc1e6432963654c190a56567afbe7
Oso-curioso <FernandoRuSe052301@outlook.com>
2026-05-11 09:16:52 -0600

    Se agrega trailingSlash

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   site: 'https://YaeloMaximoff.github.io',
   base: '/PagWebTrening',
+  trailingSlash: 'always',
   vite: {
     plugins: [tailwindcss()],
   },
